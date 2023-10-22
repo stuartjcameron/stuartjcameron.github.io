bd7b496fed7ee107866e29c87b636bca7f2014f5
Stuart Cameron <stuartjcameron@gmail.com>
2023-10-22 23:21:24 +0100

    gitignore and websetup

---
+++ websetup/make.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 15 12:15:56 2022
+
+Process data in a set of word documents, and output a single-page mini-site 
+compiling information on topics, country analytics, and statistics
+
+Also gets statistics from a file stored in the same folder, which 
+can be downloaded from UIS.
+
+@author: https://github.com/stuartjcameron
+"""
+
+#import mammoth
+#from bs4 import BeautifulSoup
+import os
+#import re
+#import json
+#import glob
+#import yaml
+#import datetime
+#import pandas as pd
+
+# Note, for some reason it seems necessary to import this explicitly
+# to make the clipboard module (just a copy of pyperclip) accessible 
+#import pandas.io.clipboard as pyperclip
+
+#import klembord
+
+from jinja2 import Environment, FileSystemLoader
+
+
+SCRIPT_PATH = os.path.realpath(__file__)
+PATH = os.path.dirname(os.path.dirname(SCRIPT_PATH))
+
+environment = Environment(loader=FileSystemLoader(f"{PATH}/websetup"))
+template = environment.get_template("template.j2")
+output = template.render(test_content="Hello there!")
+with open(f"{PATH}/index.html", "w", encoding="utf-8") as f:
+    f.write(output)
