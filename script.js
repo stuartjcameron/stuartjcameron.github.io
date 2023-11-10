bf1f32182f14041d52c397f14dd2699005e0e0d6
Stuart Cameron <stuartjcameron@gmail.com>
2023-11-10 20:59:37 +0000

    tidied js

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 const headings = document.getElementsByTagName("h2");
 const leftNav = document.getElementById("left-nav");
 const topLink = document.getElementById("top-link");
-const inlineNav = document.createElement("nav");
-inlineNav.id = "inline-nav";
-headings[0].parentNode.insertBefore(inlineNav, headings[0]);
-const anchors = new Set();
+const main = document.getElementsByTagName("main")[0];
+
+/* Add empty block at end so it shows the last section heading towards top of screen */
 const bigBlockAtEnd = document.createElement("div");
 bigBlockAtEnd.id = "big-block";
 bigBlockAtEnd.innerHTML = "&nbsp;"
-const main = document.getElementsByTagName("main")[0];
 main.appendChild(bigBlockAtEnd);
-const ul = document.createElement("ul");
-onResize();
-addEventListener("resize", onResize);
 
+/* Create inline nav for small screens */
+const inlineNav = document.createElement("nav");
+inlineNav.id = "inline-nav";
+headings[0].parentNode.insertBefore(inlineNav, headings[0]);
+
+/* Add a link for each section heading to both the left nav and the inline nav */
+const anchors = new Set();
+const ul = document.createElement("ul");
+leftNav.appendChild(ul);
+let first = true;
+const leftNavItems = [];
+for (const heading of headings) {
+    const id_ = makeAnchor(heading.textContent);
+    heading.id = id_;
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.innerHTML = heading.innerHTML;
+    a.href = "#" + id_;
+    li.appendChild(a);
+    ul.appendChild(li);
+    if (!first) {
+        inlineNav.appendChild(document.createTextNode(" | "));
+    }
+    inlineNav.appendChild(a.cloneNode(true));
+    leftNavItems.push(li);
+    first = false;
+}
+
+/* Update everything according to screen size and scroll position */
+updateLeftNav();
+onResize();
+
+/* Add listeners for scroll and resize */
+addEventListener("resize", onResize);
 let caughtScroll = false;
 addEventListener("scroll", onScroll);
 
 function onResize() {
+    //--- When user resizes screen, position the left nav correctly
     const displayMode = {"0px": "narrow", "1px": "medium", "2px": "wide"}[getComputedStyle(document.getElementById("display-mode-indicator")).width];
     if (displayMode === "wide") {
         const leftPos = Math.max(0, main.getBoundingClientRect().left - leftNav.offsetWidth) + "px";
@@ -30,6 +60,7 @@ function onResize() {
 }
 
 function onScroll() {
+    //--- Update the left nav when scrolling 
     if (!caughtScroll) {
         requestAnimationFrame(() => {
             updateLeftNav();
@@ -40,6 +71,7 @@ function onScroll() {
 }
 
 function updateLeftNav() {
+    //--- Show the current section heading as selected in the left nav 
     const y = window.scrollY;
     const nextSectionNumber = [...headings].findIndex(h => y < h.offsetTop - parseInt(getComputedStyle(h).marginTop));
     const currentSectionNumber = (nextSectionNumber === - 1 ? [...headings].length : nextSectionNumber) - 1;
@@ -50,9 +82,6 @@ function updateLeftNav() {
         leftNavItems[currentSectionNumber].classList.add("selected");
     }
 }
-
-
-leftNav.appendChild(ul);
 
 function makeAnchor(s) {
     //--- Turn any text into a usable and unique id / anchor
@@ -67,21 +96,3 @@ function makeAnchor(s) {
     anchors.add(anchor);
     return anchor;
 }
-
-let first = true;
-for (const heading of headings) {
-    const id_ = makeAnchor(heading.textContent);
-    heading.id = id_;
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    a.innerHTML = heading.innerHTML;
-    a.href = "#" + id_;
-    li.appendChild(a);
-    ul.appendChild(li);
-    if (!first) {
-        inlineNav.appendChild(document.createTextNode(" | "));
-    }
-    inlineNav.appendChild(a.cloneNode(true));
-    first = false;
-}
-const leftNavItems = [...document.querySelectorAll("#left-nav li")];
